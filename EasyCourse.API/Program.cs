b86b8e92c4a70bfea725976119197450ecce9cd0
hajduty <hajduty@outlook.com>
2025-11-06 02:00:11 +0100

    make routes lowercase and use kebab

--- EasyCourse.API/Program.cs
+++ EasyCourse.API/Program.cs
@@ -1,6 +1,10 @@
 
 using EasyCourse.API.Middleware;
 using EasyCourse.Infrastructure;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace EasyCourse.API
 {
@@ -12,10 +16,38 @@ namespace EasyCourse.API
 
             // Add services to the container.
             builder.Services.AddInfrastructure(builder.Configuration);
-            builder.Services.AddControllers();
+            // Add controller, use kebab
+            builder.Services.AddControllers(options =>
+            {
+                options.Conventions.Add(
+                    new RouteTokenTransformerConvention(new SlugifyParameterTransformer()));
+            });
+
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = "Bearer";
+                options.DefaultChallengeScheme = "Bearer";
+            })
+            .AddJwtBearer("Bearer", options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = builder.Configuration["Jwt:Issuer"],
+                    ValidAudience = builder.Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]!)
+                    ),
+                    NameClaimType = JwtRegisteredClaimNames.Sub
+                };
+            });
 
             var app = builder.Build();
 
