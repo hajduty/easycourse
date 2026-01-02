d74c502b0c40552e47315492278d5b77e0ff05c5
hajduty <hajduty@outlook.com>
2026-01-02 07:32:30 +0100

    codeproblem entity & base logic

---
+++ backend/EasyCourse.Core/Entities/CodeProblem.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace EasyCourse.Core.Entities;
+
+public class CodeProblem
+{
+    public Guid Id { get; set; }
+    public string Language { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string Description { get; set; } = null!;
+    public string StarterCode { get; set; } = null!;
+    public string? ExpectedOutput { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    public Guid CreatorId { get; set; }
+    [JsonIgnore]
+    public User Creator { get; set; } = null!;
+}
---
+++ backend/EasyCourse.Core/Interfaces/Repository/ICodeRepository.cs
@@ -0,0 +1,17 @@
+﻿using EasyCourse.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCourse.Core.Interfaces.Repository;
+
+public interface ICodeRepository
+{
+    Task<CodeProblem> CreateProblem(CodeProblem problem);
+    Task<CodeProblem> GetProblemById(Guid id);
+    Task<IEnumerable<CodeProblem>> GetAllProblems();
+    Task<CodeProblem> UpdateProblem(CodeProblem problem);
+    Task<bool> DeleteProblem(CodeProblem problem);
+}
